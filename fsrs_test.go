2bcabd86359ad19f7aa68d7d58eb2a30485f646e
Singee <git@singee.me>
2023-04-24 14:59:51 +0800

    add tests github action

--- fsrs_test.go
+++ fsrs_test.go
@@ -2,39 +2,44 @@ package fsrs
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 	"time"
 )
 
-func TestRepeat(t *testing.T) {
+func TestSimpleWorkWell(t *testing.T) {
+	Example()
+}
+
+func Example() {
 	p := DefaultParam()
 	card := NewCard()
 	now := time.Date(2022, 11, 29, 12, 30, 0, 0, time.UTC)
 	schedulingCards := p.Repeat(card, now)
 	schedule, _ := json.Marshal(schedulingCards)
-	t.Logf(string(schedule))
+	fmt.Println(string(schedule))
 
 	card = schedulingCards[Good].Card
 	now = card.Due
 	schedulingCards = p.Repeat(card, now)
 	schedule, _ = json.Marshal(schedulingCards)
-	t.Logf(string(schedule))
+	fmt.Println(string(schedule))
 
 	card = schedulingCards[Good].Card
 	now = card.Due
 	schedulingCards = p.Repeat(card, now)
 	schedule, _ = json.Marshal(schedulingCards)
-	t.Logf(string(schedule))
+	fmt.Println(string(schedule))
 
 	card = schedulingCards[Again].Card
 	now = card.Due
 	schedulingCards = p.Repeat(card, now)
 	schedule, _ = json.Marshal(schedulingCards)
-	t.Logf(string(schedule))
+	fmt.Println(string(schedule))
 
 	card = schedulingCards[Good].Card
 	now = card.Due
 	schedulingCards = p.Repeat(card, now)
 	schedule, _ = json.Marshal(schedulingCards)
-	t.Logf(string(schedule))
+	fmt.Println(string(schedule))
 }
