4175b055c41ecc99b698664ffb42979c30ea65b7
Jarrett Ye <jarrett.ye@outlook.com>
2023-07-28 11:13:15 +0800

    refactor test

--- fsrs_test.go
+++ fsrs_test.go
@@ -3,65 +3,40 @@ package fsrs
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"testing"
 	"time"
 )
 
-func TestSimpleWorkWell(t *testing.T) {
-	Example()
-	RepeatAgains()
-}
-
-func Example() {
+func TestExample(t *testing.T) {
 	p := DefaultParam()
+	p.W = Weights{1.14, 1.01, 5.44, 14.67, 5.3024, 1.5662, 1.2503, 0.0028, 1.5489, 0.1763, 0.9953, 2.7473, 0.0179, 0.3105, 0.3976, 0.0, 2.0902}
 	card := NewCard()
 	now := time.Date(2022, 11, 29, 12, 30, 0, 0, time.UTC)
+	var ivl uint64 = 0
+	var ivlList []uint64
 	schedulingCards := p.Repeat(card, now)
 	schedule, _ := json.MarshalIndent(schedulingCards, "", "    ")
 	fmt.Println(string(schedule))
 
-	card = schedulingCards[Good].Card
-	now = card.Due
-	schedulingCards = p.Repeat(card, now)
-	schedule, _ = json.MarshalIndent(schedulingCards, "", "    ")
-	fmt.Println(string(schedule))
+	var ratings = []Rating{Good, Good, Good, Good, Good, Good, Again, Again, Good, Good, Good, Good, Good}
+	var rating Rating
 
-	card = schedulingCards[Good].Card
-	now = card.Due
-	schedulingCards = p.Repeat(card, now)
-	schedule, _ = json.MarshalIndent(schedulingCards, "", "    ")
-	fmt.Println(string(schedule))
+	for i := 0; i < len(ratings); i++ {
+		rating = ratings[i]
+		card = schedulingCards[rating].Card
+		ivl = card.ScheduledDays
+		ivlList = append(ivlList, ivl)
+		now = card.Due
+		schedulingCards = p.Repeat(card, now)
+		schedule, _ = json.MarshalIndent(schedulingCards, "", "    ")
+		fmt.Println(string(schedule))
+	}
 
-	card = schedulingCards[Again].Card
-	now = card.Due
-	schedulingCards = p.Repeat(card, now)
-	schedule, _ = json.MarshalIndent(schedulingCards, "", "    ")
-	fmt.Println(string(schedule))
+	fmt.Println(ivlList)
 
-	card = schedulingCards[Good].Card
-	now = card.Due
-	schedulingCards = p.Repeat(card, now)
-	schedule, _ = json.MarshalIndent(schedulingCards, "", "    ")
-	fmt.Println(string(schedule))
-}
-
-func RepeatAgains() {
-	p := DefaultParam()
-	card := NewCard()
-	now := time.Date(2022, 11, 29, 12, 30, 0, 0, time.UTC)
-	schedulingCards := p.Repeat(card, now)
-	schedule, _ := json.MarshalIndent(schedulingCards, "", "    ")
-	fmt.Println(string(schedule))
-
-	card = schedulingCards[Again].Card
-	now = card.Due
-	schedulingCards = p.Repeat(card, now)
-	schedule, _ = json.MarshalIndent(schedulingCards, "", "    ")
-	fmt.Println(string(schedule))
-
-	card = schedulingCards[Again].Card
-	now = card.Due
-	schedulingCards = p.Repeat(card, now)
-	schedule, _ = json.MarshalIndent(schedulingCards, "", "    ")
-	fmt.Println(string(schedule))
+	want := []uint64{0, 5, 16, 43, 106, 236, 0, 0, 12, 25, 47, 85, 147}
+	if !reflect.DeepEqual(ivlList, want) {
+		t.Errorf("excepted:%v, got:%v", ivlList, want)
+	}
 }
