9274c07993e6ab4af89725511a8edf5ecf6558c9
Jarrett Ye <jarrett.ye@outlook.com>
2023-07-28 17:30:34 +0800

    Feat/update to FSRS v4 (#11)

--- params.go
+++ params.go
@@ -1,12 +1,10 @@
 package fsrs
 
-type Weights [13]float64
+type Weights [17]float64
 
 type Parameters struct {
 	RequestRetention float64 `json:"RequestRetention"`
 	MaximumInterval  float64 `json:"MaximumInterval"`
-	EasyBonus        float64 `json:"EasyBonus"`
-	HardFactor       float64 `json:"HardFactor"`
 	W                Weights `json:"Weights"`
 }
 
@@ -14,12 +12,10 @@ func DefaultParam() Parameters {
 	return Parameters{
 		RequestRetention: 0.9,
 		MaximumInterval:  36500,
-		EasyBonus:        1.3,
-		HardFactor:       1.2,
 		W:                DefaultWeights(),
 	}
 }
 
 func DefaultWeights() Weights {
-	return Weights{1, 1, 5, -0.5, -0.5, 0.2, 1.4, -0.12, 0.8, 2, -0.2, 0.2, 1}
+	return Weights{0.4, 0.6, 2.4, 5.8, 4.93, 0.94, 0.86, 0.01, 1.49, 0.14, 0.94, 2.18, 0.05, 0.34, 1.26, 0.29, 2.61}
 }
