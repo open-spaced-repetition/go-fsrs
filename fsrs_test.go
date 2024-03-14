bd339b4b45a078ca25c3b93969d8a0ebc5002161
Jarrett Ye <jarrett.ye@outlook.com>
2024-03-14 22:53:30 +0800

    Feat/update to FSRS-4.5 (#13)

--- fsrs_test.go
+++ fsrs_test.go
@@ -3,14 +3,21 @@ package fsrs
 import (
 	"encoding/json"
 	"fmt"
+	"math"
 	"reflect"
 	"testing"
 	"time"
 )
 
+func roundFloat(val float64, precision uint) float64 {
+	ratio := math.Pow(10, float64(precision))
+	return math.Round(val*ratio) / ratio
+}
+
 func TestExample(t *testing.T) {
 	p := DefaultParam()
-	p.W = Weights{1.14, 1.01, 5.44, 14.67, 5.3024, 1.5662, 1.2503, 0.0028, 1.5489, 0.1763, 0.9953, 2.7473, 0.0179, 0.3105, 0.3976, 0.0, 2.0902}
+	p.W = Weights{1.0171, 1.8296, 4.4145, 10.9355, 5.0965, 1.3322, 1.017, 0.0, 1.6243, 0.1369, 1.0321,
+		2.1866, 0.0661, 0.336, 1.7766, 0.1693, 2.9244}
 	card := NewCard()
 	now := time.Date(2022, 11, 29, 12, 30, 0, 0, time.UTC)
 	var ivlList []uint64
@@ -38,12 +45,58 @@ func TestExample(t *testing.T) {
 	fmt.Println(ivlList)
 	fmt.Println(stateList)
 
-	wantIvlList := []uint64{0, 5, 16, 43, 106, 236, 0, 0, 12, 25, 47, 85, 147}
+	wantIvlList := []uint64{0, 4, 15, 49, 143, 379, 0, 0, 15, 37, 85, 184, 376}
 	if !reflect.DeepEqual(ivlList, wantIvlList) {
-		t.Errorf("excepted:%v, got:%v", ivlList, wantIvlList)
+		t.Errorf("excepted:%v, got:%v", wantIvlList, ivlList)
 	}
 	wantStateList := []State{New, Learning, Review, Review, Review, Review, Review, Relearning, Relearning, Review, Review, Review, Review}
 	if !reflect.DeepEqual(stateList, wantStateList) {
-		t.Errorf("excepted:%v, got:%v", stateList, wantStateList)
+		t.Errorf("excepted:%v, got:%v", wantStateList, stateList)
+	}
+}
+
+func TestMemoState(t *testing.T) {
+	p := DefaultParam()
+	p.W = Weights{1.0171, 1.8296, 4.4145, 10.9355, 5.0965, 1.3322, 1.017, 0.0, 1.6243, 0.1369, 1.0321,
+		2.1866, 0.0661, 0.336, 1.7766, 0.1693, 2.9244}
+	card := NewCard()
+	now := time.Date(2022, 11, 29, 12, 30, 0, 0, time.UTC)
+
+	schedulingCards := p.Repeat(card, now)
+	var ratings = []Rating{Again, Good, Good, Good, Good, Good}
+	var ivlList = []uint64{0, 0, 1, 3, 8, 21}
+	var rating Rating
+	for i := 0; i < len(ratings); i++ {
+		rating = ratings[i]
+		card = schedulingCards[rating].Card
+		now = now.Add(time.Duration(ivlList[i]) * 24 * time.Hour)
+		schedulingCards = p.Repeat(card, now)
+	}
+	wantStability := 43.0554
+	cardStability := roundFloat(schedulingCards[Good].Card.Stability, 4)
+	wantDifficulty := 7.7609
+	cardDifficulty := roundFloat(schedulingCards[Good].Card.Difficulty, 4)
+
+	if !reflect.DeepEqual(wantStability, cardStability) {
+		t.Errorf("excepted:%v, got:%v", wantStability, cardStability)
+	}
+
+	if !reflect.DeepEqual(wantDifficulty, cardDifficulty) {
+		t.Errorf("excepted:%v, got:%v", wantDifficulty, cardDifficulty)
+	}
+}
+
+func TestNextInterval(t *testing.T) {
+	p := DefaultParam()
+	p.W = Weights{1.0171, 1.8296, 4.4145, 10.9355, 5.0965, 1.3322, 1.017, 0.0, 1.6243, 0.1369, 1.0321,
+		2.1866, 0.0661, 0.336, 1.7766, 0.1693, 2.9244}
+	var ivlList []float64
+	for i := 1; i <= 10; i++ {
+		p.RequestRetention = float64(i) / 10
+		ivlList = append(ivlList, p.nextInterval(1))
+	}
+	wantIvlList := []float64{422, 102, 43, 22, 13, 8, 4, 2, 1, 1}
+	if !reflect.DeepEqual(ivlList, wantIvlList) {
+		t.Errorf("excepted:%v, got:%v", wantIvlList, ivlList)
 	}
 }
