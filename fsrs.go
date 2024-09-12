c4552d2f4a7a841d4ea5523c74ac0995e347a289
Jarrett Ye <jarrett.ye@outlook.com>
2024-09-12 14:20:15 +0800

    Feat/get retrievability (#24)

--- fsrs.go
+++ fsrs.go
@@ -19,3 +19,13 @@ func (f *FSRS) Repeat(card Card, now time.Time) RecordLog {
 func (f *FSRS) Next(card Card, now time.Time, grade Rating) SchedulingInfo {
 	return f.scheduler(card, now).Review(grade)
 }
+
+func (f *FSRS) GetRetrievability(card Card, now time.Time) float64 {
+	if card.State == New {
+		return 0
+	} else {
+		elapsedDays := now.Sub(card.LastReview).Hours() / 24
+		retrievability := f.Parameters.forgettingCurve(elapsedDays, card.Stability)
+		return retrievability
+	}
+}
